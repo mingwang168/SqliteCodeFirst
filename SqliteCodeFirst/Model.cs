4da949483425d8d7858de346c4d2c97da5d083df
mingwang168 <mingwang0904@gmail.com>
2019-10-18 21:51:14 -0700

    Add project files.

---
+++ SqliteCodeFirst/Model.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace SqliteCodeFirst
+{
+    class Model
+    {
+        public class ClassContext : DbContext
+        {
+            public DbSet<Instructor> Instructors { get; set; }
+            public DbSet<Class> Classes { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=C:\\Users\\Minghui Wang\\source\\repos\\SqliteCodeFirst\\SqliteCodeFirst\\classes.db");
+        }
+
+        public class Instructor
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+            public string InstructorId { get; set; }
+            public string Name { get; set; }
+            public List<Class> Classes { get; } = new List<Class>();
+        }
+
+        public class Class
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int ClassId { get; set; }
+            public string Title { get; set; }
+            public string InstructorId { get; set; }
+            public Instructor Instructor { get; set; }
+        }
+
+    }
+}
---
+++ SqliteCodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static SqliteCodeFirst.Model;
+
+namespace SqliteCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ClassContext())
+            {
+                db.Add(new Instructor { InstructorId = "A00948215", Name = "Phil Weier" });
+
+                db.Add(new Class { ClassId = 1, Instructor = new Instructor { InstructorId = "A00948415", Name = "Phil Weier" }, InstructorId = "1", Title = "teacher" });
+                db.SaveChanges();
+                foreach (Instructor instructor in db.Instructors)
+                {
+                    Console.WriteLine(instructor.InstructorId + ": " + instructor.Name);
+                }
+
+
+            }
+
+        }
+    }
+}
