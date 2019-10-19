a92077cd2a8aa893cbd12166146a0122ff917166
mingwang168 <mingwang0904@gmail.com>
2019-10-18 22:44:04 -0700

    ssdad

--- SqliteCodeFirst/Program.cs
+++ SqliteCodeFirst/Program.cs
@@ -9,9 +9,9 @@ namespace SqliteCodeFirst
         {
             using (var db = new ClassContext())
             {
-                db.Add(new Instructor { InstructorId = "A00948215", Name = "Phil Weier" });
+                db.Add(new Instructor { InstructorId = "A01968215", Name = "Phil Weier" });
 
-                db.Add(new Class { ClassId = 1, Instructor = new Instructor { InstructorId = "A00948415", Name = "Phil Weier" }, InstructorId = "1", Title = "teacher" });
+                //db.Add(new Class { ClassId = 1, Instructor = new Instructor { InstructorId = "A01968415", Name = "Phil Weier" }, InstructorId = "3", Title = "teacher" });
                 db.SaveChanges();
                 foreach (Instructor instructor in db.Instructors)
                 {
